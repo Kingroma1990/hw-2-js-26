aeaf666cc9049e8ecb9aabd6e9583002b6e0ab5a
Roma <king_roma@ukr.net>
2019-12-08 13:20:18 +0200

    2

--- js/hw.2.1.js
+++ js/hw.2.1.js
@@ -1,10 +1,11 @@
 "use strict";
+let number = 1;
 const logItems = function(array) {
   for (const element of array) {
-    let number = array.indexOf(element) +1;
-    console.log(`${number} - ${element}`);
-  }
     
+    console.log(`${number} - ${element}`);
+    number += 1;
+  }
 };
 console.log(logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]));
-console.log(logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]));
+// console.log(logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]));
